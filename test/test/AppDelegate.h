b8a9dfa77b19334e55ecf3d79af137e5a3f305a1
吴金林 <kolin@wujinlindeMacBook-Pro.local>
2015-06-02 16:29:21 +0800

    测试项目

---
+++ test/test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  test
+//
+//  Created by 吴金林 on 15/6/2.
+//  Copyright (c) 2015年 winainfo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
